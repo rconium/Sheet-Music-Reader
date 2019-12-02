7096bde7e5d3528e4c9364d2262ebb0f75d8a7fd
Eric Zavala <30513993+EricZavala05@users.noreply.github.com>
2019-12-01 23:17:49 -0600

    Working multi scale multi template matching

--- multi.py
+++ multi.py
@@ -5,15 +5,11 @@ import imutils
 import glob
 import cv2
 
+# templates list
 note_files = [
-    "template/c.png", 
-    "template/d.png",
-    "template/e.png", 
-    "template/f.png",
-    "template/g.png",
-    "template/a.png", 
-    "template/b.png"]
+    "template/f.png"]
 
+# description of templates above
 n = [
 	'c',
 	'd',
@@ -23,15 +19,15 @@ n = [
 	'a',
 	'b']
 
-
-image = cv2.imread("test.png")
+# image being analyzed
+image = cv2.imread("images/test.png")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# index for template descriptors
 i = 0
 for t in note_files:
-  # load the image image, convert it to grayscalse, and detect edges
-  template = cv2.imread(t)
-  template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-  template = cv2.Canny(template, 50, 200)
+  # load the image image, convert it to grayscalse
+  template = cv2.imread(t, 0)
   (tH, tW) = template.shape[:2]
 
   # loop over the images to find the template in
@@ -40,7 +36,7 @@ for t in note_files:
   found = None
 
   # loop over the scales of the image
-  for scale in np.linspace(0.2, 1.0, 20)[::-1]:
+  for scale in np.linspace(0.2, 1.5, 80)[::-1]:
     # resize the image according to the scale, and keep track
     # of the ratio of the resizing
     resized = imutils.resize(gray, width = int(gray.shape[1] * scale))
@@ -51,27 +47,27 @@ for t in note_files:
     if resized.shape[0] < tH or resized.shape[1] < tW:
       break
 
-    # detect edges in the resized, grayscale image and apply template
-    # matching to find the template in the image
-    edged = cv2.Canny(resized, 50, 200)
-    result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
+    # apply template matching to find the template in the image
+    result = cv2.matchTemplate(resized, template, cv2.TM_CCOEFF_NORMED)
     (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
+    # threshold for determining if there is a match
+    loc = np.where(result >= 0.9)
 
     # if we have found a new maximum correlation value, then update
     # the bookkeeping variable
     if found is None or maxVal > found[0]:
-      found = (maxVal, maxLoc, r)
+      found = (maxVal, maxLoc, r, loc)
 
   # unpack the bookkeeping variable and compute the (x, y) coordinates
   # of the bounding box based on the resized ratio
-  (_, maxLoc, r) = found
-  (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
-  (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
-
-  # draw a bounding box around the detected result and display the image
-  image = cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
-  cv2.putText(image, n[i], (startX, startY), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,0,0), 2)
-  i = i + 1
-
+  (_, maxLoc, r, loc) = found
   
-cv2.imwrite("result.png", image)
+  for (x,y) in zip(loc[1], loc[0]):
+    (startX, startY) = (int(x * r), int(y * r))
+    (endX, endY) = (int((x + tW) * r), int((y + tH) * r))
+
+    # draw a bounding box around the detected result and display the image
+    image = cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+    cv2.putText(image, n[i], (startX, startY), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,0,0), 2)
+  i = i + 1
+cv2.imwrite("multi-result.png", image)
